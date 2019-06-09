f8575312ff93aac47c8628bba119aa4399620302
MrLambert13 <Lam_13@mail.ru>
2019-06-09 17:14:58 +0300

    mini css extract

--- src/user.js
+++ src/user.js
@@ -1,3 +1,5 @@
+import './user.css';
+
 export function sayHi(firstName, lastName) {
     return `Hi ${firstName} ${lastName}`;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractLoader = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractLoader.loader,
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractLoader({
+            filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
     ]
-  }
 
 };
