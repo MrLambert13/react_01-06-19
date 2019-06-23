6e33f4c00036e2ec26b15cf3ed04e24a8b1b017b
MrLambert13 <Lam_13@mail.ru>
2019-06-23 15:49:48 +0300

    Auth complete

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -2,45 +2,47 @@ import './Auth.sass';
 
 import React, {Component} from 'react';
 
-//Sallie31@gmail.com
-//Cassandra21@yahoo.com
 export class Auth extends Component {
     state = {
         username: '',
         password: '',
-    };
+    }
 
-    handleSignInClick = () => {
+    handleSignIn = () => {
         const {username, password} = this.state;
         const {onSuccess} = this.props;
-
         fetch('http://localhost:8888/auth', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            method: 'post',
-            body: JSON.stringify({username, password}),
+            body: JSON.stringify({username, password})
         })
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Wrong credentials');
+                }
+                return response.json();
+            })
             .then(data => onSuccess(data.token));
-    };
+    }
 
-    handleChangeInput = ({target: {name, value}}) => {
+    handleTextChange = ({target: {name, value}}) => {
         this.setState({
             [name]: value,
-        })
-    };
+        });
+    }
 
     render() {
         const {username, password} = this.state;
         return (
             <div className="Auth">
                 <h1>Login</h1>
-                <input onChange={this.handleChangeInput} name="username" type="text" value={username}
-                       placeholder="E-mail"/>
-                <input onChange={this.handleChangeInput} name="password" type="password" value={password}
-                       placeholder="Password"/>
-                <button onClick={this.handleSignInClick}>Sign In</button>
+                <input onChange={this.handleTextChange} name="username" type="text" value={username}
+                       placeholder="E-mail"/><br/>
+                <input onChange={this.handleTextChange} name="password" type="password" value={password}
+                       placeholder="Password"/><br/>
+                <button onClick={this.handleSignIn}>Sign In</button>
             </div>
         );
     }
